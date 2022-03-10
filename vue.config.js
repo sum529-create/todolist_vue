e9072cb1aa63d8ac2009480db8f4b0399b11a6b5
smnoh <smnoh@ui-lab.co.kr>
2022-03-10 13:01:57 +0900

    modify config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  //   outputDir: "./docs",
-  publicPath: "/todolist_vue",
+  outputDir: "./docs",
+  publicPath: process.env.NODE_ENV === "production" ? "/todolist_vue/" : "/",
 };
